5b17797b609ac96d00a2131599a8889e4c05184a
Hugh Cunningham <hughec@cs.stanford.edu>
2014-06-05 12:44:01 -0700

    Updated baselineclassifer.py to run several baseline tests.
    A naive model of link strength between reviews of the same business by a pair of friends, which assigns a label based on the majority classification among friends who had previously reviewed the business is implemented.  This is coupled with an added randomized class assignment and naive Bayes classifier where there is no freind review to base classification on from the training set.  Naive influence improves classification over randomization, but hurts performance when coupled with naive Bayes.

--- baselineclassifier.py
+++ baselineclassifier.py
@@ -3,38 +3,52 @@
 import readyelp
 import cleanyelp
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 import numpy
 import random
 
+def random_class():
+    assignment = random.random()
+    if assignment <= 0.5:
+        return "positive"
+    else:
+        return "negative"
 
 def influence_baseline(train_reviews, test_reviews, user_dict):
     """ Implements a baseline classifier that uses only review influencers.  That is, for each review in the test set, the label assigned is the majority label from the influencers of that review in the training set where an influencer is defined as a review of the same business at an earlier date from a user who is friends with the user who created the given test review. """
     Y_predict = []
-    randomized_labels = 0
-    non_random = 0
+    total_train_influencers = 0
+    total_test_influences = 0
+    no_influencers = 0
     for review_id in test_reviews:
         review = test_reviews[review_id]
-        influencer_list = cleanyelp.find_influencers(review, train_reviews, user_dict)
-        if len(influencer_list) == 0:
-            Y_predict.append("UNKNOWN")
-            continue
-        else:
-            non_random += 1
-            influence_sum = 0
-            for influencer_id in influencer_list:
+        influencer_list = review["friend_reviews_of_business"]
+        influence_sum = 0
+        for influencer_id in influencer_list:
+            if influencer_id in train_reviews:
                 influencer = train_reviews[influencer_id]
-                if influencer["rating"] == "negative":
-                    influence_sum -= 1
-                else:
-                    influence_sum += 1
-            if influence_sum < 0:
-                Y_predict.append("negative")
+            elif influencer_id in test_reviews:
+                # influencer = test_reviews[influencer_id]
+                continue
             else:
-                Y_predict.append("positive")
+                print "Influencer not in either set?"
+                continue
+            if influencer["rating"] == "negative":
+                influence_sum -= 1
+            else:
+                influence_sum += 1
+        if influence_sum < 0:
+            Y_predict.append("negative")
+        elif influence_sum > 0:
+            Y_predict.append("positive")
+        else:
+            Y_predict.append("UNKNOWN")
+    print "Total influencers in training set:", total_train_influencers
+    print "Total influencers in test set:", total_test_influences
+    print "Test reviews with no influencers:", no_influencers
     return Y_predict
 
 def bag_of_words_baseline(train_reviews, test_corpus):
@@ -49,7 +63,7 @@ def bag_of_words_baseline(train_reviews, test_corpus):
     vectorizer = CountVectorizer(stop_words = 'english')
     X_train = vectorizer.fit_transform(train_corpus)
 
-    # clf = SVC(class_weight = 'auto').fit(X_train, Y_train)
+    # clf = LinearSVC(class_weight = 'auto').fit(X_train, Y_train)
     # clf = LogisticRegression().fit(X_train, Y_train)
     clf = MultinomialNB().fit(X_train, Y_train)
 
@@ -66,21 +80,36 @@ def main():
     klass_list = ["negative", "positive"]
     test_corpus = []
     gold_labels = []
+    Y_random = []
     for review_id in test_reviews:
         review = test_reviews[review_id]
         test_corpus.append(review["text"])
         gold_labels.append(review["rating"])
+        Y_random.append(random_class())
+
+    print "Random model metrics:"
+    print metrics.classification_report(gold_labels, Y_random, target_names = klass_list)
 
     Y_bag_of_words = bag_of_words_baseline(train_reviews, test_corpus)
     print "Bag of words baseline model metrics:"
     print metrics.classification_report(gold_labels, Y_bag_of_words, target_names = klass_list)
 
+    Y_random_influence = []
+    Y_bow_influence = []
     Y_influence = influence_baseline(train_reviews, test_reviews, user_dict)
     for i in range(len(Y_influence)):
         if Y_influence[i] == "UNKNOWN":
-            Y_influence[i] = Y_bag_of_words[i]
-    print "Influence baseline model metrics:"
-    print metrics.classification_report(gold_labels, Y_influence, target_names = klass_list)
+            Y_bow_influence.append(Y_bag_of_words[i])
+            Y_random_influence.append(Y_random[i])
+        else:
+            Y_bow_influence.append(Y_influence[i])
+            Y_random_influence.append(Y_influence[i])
+
+    print "Random influence baseline model metrics:"
+    print metrics.classification_report(gold_labels, Y_random_influence, target_names = klass_list)
+
+    print "Bag-of-words influence baseline model metrics:"
+    print metrics.classification_report(gold_labels, Y_bow_influence, target_names = klass_list)
 
 
 
