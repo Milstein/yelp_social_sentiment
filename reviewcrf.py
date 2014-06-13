0654b4f5930856db5648eab235be6feb2a477d15
Hugh Cunningham <hughec@cs.stanford.edu>
2014-06-12 21:51:03 -0700

    Experimented with different feature functions in CRF, to no positive effect.
    Working with features involving cosine similarity of documents to one another or to each class does not seem to help performance.  The CRF outputs that all pairs of reviews should receive the same classification.  I believe that this is largely due to lack of training examples from reviews sequences.  I will try working with CRF training on a sequence of all reviews of the same business.  Further development of feature functions for the CRF will also be worthwhile.

--- reviewcrf.py
+++ reviewcrf.py
@@ -2,7 +2,10 @@
 
 import pycrfsuite
 import readyelp
+import cleanyelp
 import random
+import numpy
+
 
 def train_crf(train_reviews, user_dict):
     """ Given a dictionary of training reviews and a dictionary of user objects, trains a Linear-chain CRF model to tag pairs of reviews as either members of the same class (["1", "1"]) or different classes (["1", "0"]).  Features for the CRF consist of the date and text of each review. """
@@ -14,13 +17,10 @@ def train_crf(train_reviews, user_dict):
             if friend_review_id not in train_reviews: continue
             friend_review = train_reviews[friend_review_id]
             if friend_review["date"] <= train_review["date"]:
-                train_user = user_dict[train_review["user_id"]]
-                train_user_friend_set = set(train_user["friends"])
-                friend_user = user_dict[friend_review["user_id"]]
-                friend_user_friend_set = set(friend_user["friends"])
-                mutual_friends = len(train_user_friend_set.intersection(friend_user_friend_set))
-                friend_item = {"id":friend_review["review_id"], "date":friend_review["date"], "mutual_friends":mutual_friends}
-                train_item = {"id":train_review["review_id"], "date":train_review["date"], "mutual_friends":mutual_friends}
+                friend = user_dict[friend_review["user_id"]]
+                user = user_dict[train_review["user_id"]]
+                friend_item = {"id":friend_review["user_id"], "friends":len(friend["friends"]), "text":friend_review["text"]}
+                train_item = {"id":train_review["user_id"], "friends":len(user["friends"]), "text":train_review["text"]}
                 xseq = [friend_item, train_item]
                 # If the ratings are the same between reviews, both are labeled as "1" in the Y sequence, otherwise the training review is labeled as "0".
                 # This allows for the training of a CRF that determines the probability that the latter review receives the same label as the former - the strength of the link between reviews.
@@ -37,6 +37,8 @@ def crftag_probabilities(test_reviews, user_dict):
     probabilities = {}
     tagger = pycrfsuite.Tagger()
     tagger.open("reviewcrfmodel")
+    num_same = 0
+    num_diff = 0
     for test_id in test_reviews:
         test_review = test_reviews[test_id]
         friend_reviews_of_business = test_review["friend_reviews_of_business"]
@@ -44,22 +46,21 @@ def crftag_probabilities(test_reviews, user_dict):
             if friend_review_id not in test_reviews: continue
             friend_review = test_reviews[friend_review_id]
             if friend_review["date"] <= test_review["date"]:
-                test_user = user_dict[test_review["user_id"]]
-                test_user_friend_set = set(test_user["friends"])
-                friend_user = user_dict[friend_review["user_id"]]
-                friend_user_friend_set = set(friend_user["friends"])
-                mutual_friends = len(test_user_friend_set.intersection(friend_user_friend_set))
-                friend_item = {"id":friend_review["review_id"], "date":friend_review["date"], "mutual_friends":mutual_friends}
-                test_item = {"id":test_review["review_id"], "date":test_review["date"], "mutual_friends":mutual_friends}
+                friend = user_dict[friend_review["user_id"]]
+                user = user_dict[test_review["user_id"]]
+                friend_item = {"id":friend_review["user_id"], "friends":len(friend["friends"]), "text":friend_review["text"]}
+                test_item = {"id":test_review["user_id"], "friends":len(user["friends"]), "text":test_review["text"]}
                 xseq = [friend_item, test_item]
                 yseq = tagger.tag(xseq)
                 # The probability of the labeling yseq, given the input xseq
                 prob_y = tagger.probability(yseq)
-                probabilities[(friend_review_id, test_id)] = random.random()
-                # if yseq[1] == "1":
-                #     probabilities[(friend_review_id, test_id)] = prob_y
-                # else:
-                #     probabilities[(friend_review_id, test_id)] = 1 - prob_y
+                if yseq[1] == "1":
+                    probabilities[(friend_review_id, test_id)] = prob_y
+                    num_same += 1
+                else:
+                    probabilities[(friend_review_id, test_id)] = 1 - prob_y
+                    num_diff += 1
+    print "Number of same, differnt from CRF:", num_same, num_diff
     return probabilities
 
 
