891afa070fe187a51eb5d2f7d23d30787a99d78b
Hugh Cunningham <hughec@cs.stanford.edu>
2014-06-12 21:49:37 -0700

    Reworked the arrnagment of methods in classifyyelp to allow better data splits.

--- classifyyelp.py
+++ classifyyelp.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 
 import readyelp
+import cleanyelp
 import baselineclassifier
 import reviewcrf
 import reviewgraph
 from sklearn import metrics
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 def main():
+
+    ## Only call the below once, when data needs to be cleaned and split ##
+    cleanyelp.split_data_by_business(0.75)
+    #######################################################################
+
     train_reviews = readyelp.read_reviews_to_dict("./train_reviews.json")
     test_reviews = readyelp.read_reviews_to_dict("./test_reviews.json")
     user_dict = readyelp.read_users_to_dict("./users_limited.json")
@@ -21,11 +28,9 @@ def main():
     pair_str = reviewcrf.crftag_probabilities(test_reviews, user_dict)
     print "Pair strengths calculated."
     # Build review graph
-    rgraph = reviewgraph.build_graph(klass_list, test_reviews, ind_pref, pair_str)
-    print "Graph built."
-    # Make min-cut classification
-    min_cut_classes = reviewgraph.mincutclassification(rgraph)
+    min_cut_classes = reviewgraph.build_graph(klass_list, test_reviews, ind_pref, pair_str)
     print "Graph cut."
+    # Make min-cut classification
     Y_gold = []
     Y_predict = []
     for test_id in test_reviews:
@@ -36,7 +41,9 @@ def main():
         else:
             Y_predict.append("negative")
 
-    print metrics.classification_report(Y_gold, Y_predict, target_names = klass_list)
+    classification_metrics = metrics.classification_report(Y_gold, Y_predict, target_names = klass_list)
+
+    print classification_metrics
 
 
 if __name__ == "__main__":
